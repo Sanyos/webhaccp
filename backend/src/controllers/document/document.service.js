6ccc916afe825d237c6778b7617dbfafafc0e59f
Garamvölgyi Emese <garamvolgyie@molaris.local>
2022-07-27 11:57:00 +0200

    fix documents

--- backend/src/controllers/document/document.service.js
+++ backend/src/controllers/document/document.service.js
@@ -4,8 +4,8 @@ exports.getAllByCompanyId = (companyId) => {
   const selectQuery = `
   SELECT * 
   FROM documents 
-  WHERE document_company_id = $1`;
-  return pool.query(selectQuery, [companyId]);
+  `;
+  return pool.query(selectQuery, []);
 };
 
 exports.getById = (id) => {

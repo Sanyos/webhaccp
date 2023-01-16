e83fe7abf2d11f6ace7a5d016d602335db10c1eb
Teleki Sándor <sanyos07@gmail.com>
2023-01-16 18:55:03 +0100

    jogi dolgok

--- frontend/src/app/feature/companies/component/documents-table/documents-table.component.ts
+++ frontend/src/app/feature/companies/component/documents-table/documents-table.component.ts
@@ -13,6 +13,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { PaymentApiService } from 'src/app/core/api/payment-api/payment-api.service';
 import { prices } from 'src/app/core/config/prices-config';
 import { HaccpModel } from 'src/app/core/model/haccp.model';
+import { SweetAlertPopupService } from 'src/app/core/services/sweet-alert-popup/sweet-alert-popup.service';
 
 @Component({
   selector: 'app-documents-table',
@@ -31,7 +32,7 @@ export class DocumentsTableComponent implements OnInit {
   @Output() downloadHaccpEvent: EventEmitter<any> = new EventEmitter();
   @Output() downloadCertificateEvent: EventEmitter<any> = new EventEmitter();
 
-  constructor(private readonly paymentApiService: PaymentApiService) {
+  constructor(private readonly paymentApiService: PaymentApiService, private readonly sweetAlertPopupService: SweetAlertPopupService) {
     this.dataSource = new MatTableDataSource();
   }
 
@@ -91,17 +92,29 @@ export class DocumentsTableComponent implements OnInit {
   }
 
   startPayment(element: HaccpModel): void {
-    const body: any = {
-      amount: this.getPrice(element),
-      transactionId: element.haccp_transaction_id,
-      haccpId: element.haccp_id,
-      userEmail: element.haccp_user_email,
-    };
-    this.paymentApiService.startTransaction(body).subscribe({
-      next: (res) => {
-        window.open(res.paymentUrl);
-      },
-      error: (err) => console.log(err),
-    });
+    this.sweetAlertPopupService
+        .openConfirmPopup(`Tudomásul veszem, hogy a PRÉMIUM CSOPORT Szolgáltató KFT. ( 2045
+          Törökbálint, Malom dűlő 40. ) adatkezelő által a
+          https://www.webhaccp.hu/ felhasználói adatbázisában tárolt alábbi
+          személyes adataim átadásra kerülnek az OTP Mobil Kft., mint
+          adatfeldolgozó részére. Az adatkezelő által továbbított adatok köre az
+          alábbi: e-mail cím.`)
+        .then((result) => {
+          if (result.isConfirmed) {
+            const body: any = {
+              amount: this.getPrice(element),
+              transactionId: element.haccp_transaction_id,
+              haccpId: element.haccp_id,
+              userEmail: element.haccp_user_email,
+            };
+            this.paymentApiService.startTransaction(body).subscribe({
+              next: (res) => {
+                window.open(res.paymentUrl);
+              },
+              error: (err) => console.log(err),
+            });
+          }
+        });
+    
   }
 }

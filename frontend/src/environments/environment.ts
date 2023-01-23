b62e467dc33159e4278bd63e91e1942d8a6eec7a
Teleki Sándor <sanyos07@gmail.com>
2023-01-23 11:18:12 +0100

    fix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://webhaccp.hu/api',
-  apiUrl: 'http://localhost:3000' ,
+  apiUrl: 'https://webhaccp.hu/api',
+  //apiUrl: 'http://localhost:3000' ,
   premiumUrl: 'https://www.premiumcsoport.hu/',
 };
 

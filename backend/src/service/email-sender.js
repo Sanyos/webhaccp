a1d79f9a67c7504bcb6ee729b200a483198417f8
Mesi93 <emese.garamvolgyi93@gmail.com>
2023-01-27 12:13:26 +0100

    email sender fix

--- backend/src/service/email-sender.js
+++ backend/src/service/email-sender.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const createError = require("http-errors");
 const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
 
 exports.sendEmail = (userEmail, content) => {
   const transporter = nodemailer.createTransport({
